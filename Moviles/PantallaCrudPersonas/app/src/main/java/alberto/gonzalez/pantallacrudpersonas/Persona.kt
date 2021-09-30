ca7b83205076f3c4f628b32ed545f4ef60cc8710
2dam <sintra1999@hotmail.es>
2021-09-30 14:21:42 +0200

    Empezado moviles 2nd proyecto

--- Moviles/PantallaCrudPersonas/app/src/main/java/alberto/gonzalez/pantallacrudpersonas/Persona.kt
+++ Moviles/PantallaCrudPersonas/app/src/main/java/alberto/gonzalez/pantallacrudpersonas/Persona.kt
@@ -2,9 +2,6 @@ package alberto.gonzalez.pantallacrudpersonas
 
 import java.time.LocalDateTime
 
-class Persona {
 
-    data class Persona(var id: Int?, var name: String?, var date: Int?) {
 
-    }
-}
+    data class Persona(var id: Int, var name: String, var date: Int)

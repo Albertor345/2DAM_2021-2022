845561248a5e8f4aedb235203bb6e3c2704db6f2
Alberto <sintra1999@hotmail.es>
2021-10-03 23:31:25 +0200

    Crud personas landscape añadido

--- Moviles/PantallaCrudPersonas/app/src/main/java/alberto/gonzalez/pantallacrudpersonas/MainActivity.kt
+++ Moviles/PantallaCrudPersonas/app/src/main/java/alberto/gonzalez/pantallacrudpersonas/MainActivity.kt
@@ -2,6 +2,7 @@ package alberto.gonzalez.pantallacrudpersonas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.RadioGroup

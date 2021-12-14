f8131bf4e3565e52c426e43cfb6ce955826a91da
Alberto <sintra1999@hotmail.es>
2021-12-14 11:14:26 +0100

    .

--- AD/Examen_1EVA_AD_AlbertoGonzalez/src/main/java/utils/Constantes.java
+++ AD/Examen_1EVA_AD_AlbertoGonzalez/src/main/java/utils/Constantes.java
@@ -8,24 +8,24 @@ public class Constantes {
             "where id_student = ?";
 
     public final static String SELECT_ALL_SUBJECTS_FROM_STUDENT = "select * " +
-            "from subjects " +
-            "where id in (select id_subject " +
-            "from students_grades " +
-            "inner join subjects s on students_grades.id_subject = s.id " +
-            "where id_student = ? " +
-            "group by id_subject, name, id_teacher)";
+            "from subjects where id in (" +
+            "select id_subject from students_grades " +
+            "where id_student = ?" +
+            ")";
 
 
     public final static String INSERT_STUDENT_GRADE = "insert into students_grades " +
             "(id_student, id_subject, grade, attempt) " +
             "values (?, ?, ?, ?)";
 
-    public final static String SELECT_ALL_FAILED_STUDENTS = "select id_student, id_subject, max(attempt) as attempt " +
-            "from students_grades " +
-            "inner join students s on students_grades.id_student = s.id " +
-            "inner join subjects s2 on students_grades.id_subject = s2.id " +
-            "where grade < 5 " +
-            "group BY id_student, id_subject";
+    public final static String SELECT_ALL_FAILED_STUDENTS = "select name, id_subject, attempt, grade " +
+            "from students_grades as sg " +
+            "inner join students s on sg.id_student = s.id " +
+            "where attempt in (select max(attempt) as max " +
+            "                  from students_grades sg2 " +
+            "                           inner join students on sg.id_student = sg2.id_student " +
+            "                  group by id_student, id_subject) " +
+            "  and grade < 5;";
 
    /* "select id_student, id_subject, max(attempt) as mAttempt, max(grade) < 5 as pass
     from students_grades
@@ -39,10 +39,8 @@ public class Constantes {
 
     public final static String GET_STUDENT = "select * from students where id = ?";
 
-    public final static String DELETE_STUDENT = "delete from students where id = ?";
-
     public final static String DELETE_STUDENT_GRADES = "delete from students_grades where id_student = ?";
 
-
+    public final static String DELETE_STUDENT = "delete from students where id = ?";
 
 }
